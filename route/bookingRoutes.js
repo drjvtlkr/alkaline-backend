b9fceeb23d40b2b625c169b942c7d2c3121efa9e
dheeraj <dheerajrvithalkar@gmail.com>
2024-07-05 19:04:09 +0530

    All APIs done and dusted

--- route/bookingRoutes.js
+++ route/bookingRoutes.js
@@ -2,10 +2,14 @@ import express from "express";
 import {
   afterPaymentofBooking,
   getAllBookings,
+  getAllBookingsBetweenDates,
   getBookingById,
+  getBookingByStatus,
+  getBookingByCustomerId,
   initiateBooking,
   markBookingComplete
 } from "../controller/bookingController.js";
+
 
 const bookingRouter = express.Router();
 
@@ -14,5 +18,8 @@ bookingRouter.route("/:id/afterPayment").post(afterPaymentofBooking);
 bookingRouter.route("/getAllBookings").get(getAllBookings);
 bookingRouter.route("/getBookingById/:id").get(getBookingById);
 bookingRouter.route("/complete/:id").post(markBookingComplete);
+bookingRouter.route("/getBookingByStatus/:status").get(getBookingByStatus)
+bookingRouter.route("/getBookingByCustomer/:id").get(getBookingByCustomerId)//this has pagination
+bookingRouter.route("/getAllBookingsBetweenDatesPagination").get(getAllBookingsBetweenDates);
 
 export default bookingRouter;
